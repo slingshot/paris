f7984189507bb4a3f7a54ffe9a7a04c4a07f2198
Sanil Chawla <sanil@slingshot.fm>
2023-03-28 18:23:32 -0700

    Entry point generation

---
+++ scripts/generateEntry.js
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const fs = require('fs');
+
+const baseDir = path.resolve(process.cwd());
+
+const componentDirectory = `${baseDir}/src/stories`;
+
+// Directories to intentionally ignore
+const ignoreDirectories = [
+    'assets',
+];
+
+const run = async () => {
+    // Iterate through the directory and get a list of subdirectories
+    const subDirectories = fs
+        .readdirSync(componentDirectory, { withFileTypes: true })
+        .filter((dirent) => dirent.isDirectory())
+        .map((dirent) => dirent.name)
+        .filter((directory) => !ignoreDirectories.includes(directory));
+
+    // Generate entry point file
+    const entryPointFile = `${baseDir}/index.ts`;
+    const entryPointCode = subDirectories
+        .map((subDirectory) => `export * from './src/stories/${subDirectory}';`)
+        .join('\n')
+        .concat('\n');
+
+    // Write the entry point file
+    await fs.promises.writeFile(entryPointFile, entryPointCode, { encoding: 'utf8' });
+
+    return '✅  Entry point file generated.';
+};
+
+run().then(console.log).catch(console.error);
