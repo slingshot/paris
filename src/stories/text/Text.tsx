4a106d359f534940044a9c145ca6f7b86514bd10
Samuel Escapa Otani <samuel@slingshot.fm>
2023-06-06 16:14:35 -0700

    refactor: Separate global values generation

--- src/stories/text/Text.tsx
+++ src/stories/text/Text.tsx
@@ -6,8 +6,9 @@ import styles from './Text.module.scss';
 import type { LightTheme } from '../theme';
 
 export type TextElement = 'p' | 'span' | 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'label' | 'legend' | 'caption' | 'small';
-export type FontWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 'normal' | 'bold' | 'lighter' | 'bolder' | 'inherit' | 'initial' | 'revert' | 'revert-layer' | 'unset';
-export type FontStyle = 'normal' | 'italic' | 'oblique';
+export type GlobalCSSValues = 'inherit' | 'initial' | 'revert' | 'revert-layer' | 'unset';
+export type FontWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 'normal' | 'bold' | 'lighter' | 'bolder' | GlobalCSSValues;
+export type FontStyle = 'normal' | 'italic' | 'oblique' | GlobalCSSValues;
 
 export type TextProps<T extends TextElement = 'span'> = {
     /**
